59f1b44cc21ef63f7b219e55a5e0bdc559d9d903
SunDoge <384813529@qq.com>
2019-06-25 16:51:43 +0800

    don't access inner args

--- typeargs/__init__.py
+++ typeargs/__init__.py
@@ -6,11 +6,11 @@ __version__ = '0.1.0'
 
 @dataclass
 class TypeArgs:
-    args: argparse.Namespace
+    _args: argparse.Namespace
 
     def __post_init__(self):
 
         for key, value in self.__dict__.items():
             if isinstance(value, argparse.Action):
                 dest = value.dest
-                self.__dict__[key] = self.args.__dict__[dest]
+                self.__dict__[key] = self._args.__dict__[dest]
