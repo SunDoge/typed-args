3e96a16ade7e08e18f2860b18c6dbe70c8697500
SunDoge <384813529@qq.com>
2019-06-25 16:22:17 +0800

    init

---
+++ setup.py
@@ -0,0 +1,38 @@
+import io
+import re
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    with io.open(
+            os.path.join(os.path.dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+VERSION = find_version('flame', '__init__.py')
+
+requirements = [
+    'dataclasses'
+]
+
+setup(
+    name='typeargs',
+    version=VERSION,
+    author='SunDoge',
+    packages=find_packages(exclude=['test']),
+    install_requires=requirements,
+)
---
+++ typeargs/__init__.py
@@ -0,0 +1,22 @@
+import argparse
+from dataclasses import dataclass
+
+__version__ = '0.1.0'
+
+
+@dataclass
+class TypeArgs:
+    parser: argparse.ArgumentParser
+    parse_known_args: bool = True
+
+    def __post_init__(self):
+
+        if self.parse_known_args:
+            args, _ = self.parser.parse_known_args()
+        else:
+            args = self.parser.parse_args()
+
+        for key, value in self.__dict__.items():
+            if isinstance(value, argparse.Action):
+                dest = value.dest
+                self.__dict__[key] = args.__dict__[dest]
